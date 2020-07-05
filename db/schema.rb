ae664837ad700b2f5679d8d6cadb504d50c84124
lucky-ponta <soraali1225@gmail.com>
2020-07-05 12:20:32 +0900

    kadai

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_021957) do
+ActiveRecord::Schema.define(version: 2020_07_05_024215) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2020_07_05_021957) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
